720ab578e3a5d76b1477620eeb9b610160171fd9
Aalok Parkash <aalokparkash@Aaloks-MBP.home>
2024-06-18 12:19:03 +0200

    finish GPT2 implementation

--- gpt2/gpt2.py
+++ gpt2/gpt2.py
@@ -2,16 +2,50 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-
+import math
 #-----------------
 
 @dataclass
 class GPTConfig:
-    block_size: int = 256
-    vocab_size: int = 65
-    n_layer : int = 6
-    n_head : int = 6
-    n_embd: int = 384
+    block_size: int = 1024
+    vocab_size: int = 50257 # 256 tokens + 50K merges + 1 <end> token
+    n_layer : int = 12
+    n_head : int = 12
+    n_embd: int = 768
+
+class CausalSelfAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        # Let's build Q,K,V in one go!
+        self.c_attn = nn.Linear(config.n_embd, 3*config.n_embd)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        # regularization
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                             .view(1,1,config.block_size, config.block_size))
+
+    def forward(self, x):
+        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        # nh is "number of heads", hs is "head size", and C (number of channels) = nh * hs
+        # e.g. in GPT-2 (124M), n_head=12, hs=64, so nh*hs=C=768 channels in the Transformer
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2) # each q/k/v is going to be (B,T,C)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+
+        # attention
+        att = (q @ k.transpose(-2,-1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        y = att @ v
+        y = y.transpose(1,2).contiguous().view(B,T,C)
+        y = self.c_proj(y)
+        return y
+
 
 class MLP(nn.Module):
     def __init__(self, config):
